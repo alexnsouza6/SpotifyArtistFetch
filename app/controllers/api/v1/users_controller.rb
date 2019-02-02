79aebbc8b231569e3df8d27cb148189327f5134d
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-02 20:41:12 -0200

    refactor: encapsulating http requests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,23 +3,9 @@ class Api::V1::UsersController < ApplicationController
     if params[:error]
       render json: { error: params[:error] }, status: 422
     else
-      body = {
-        grant_type: 'authorization_code',
-        code: params[:code],
-        redirect_uri: ENV['REDIRECT_URI'],
-        client_id: ENV['CLIENT_ID'],
-        client_secret: ENV['CLIENT_SECRET']
-      }
+      auth_params = JSON.parse(auth_credentials.body)
 
-      auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
-
-      auth_params = JSON.parse(auth_response.body)
-
-      header = {
-        Authorization: "Bearer #{auth_params['access_token']}"
-      }
-
-      user_response = RestClient.get('https://api.spotify.com/v1/me', header)
+      user_response = user_credentials(auth_params)
 
       user_params = JSON.parse(user_response.body)
 
@@ -41,5 +27,26 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: { error: { msg: "There's no users" } }, status: 422
     end
+  end
+
+  protected
+  def auth_credentials
+      body = {
+        grant_type: 'authorization_code',
+        code: params[:code],
+        redirect_uri: ENV['REDIRECT_URI'],
+        client_id: ENV['CLIENT_ID'],
+        client_secret: ENV['CLIENT_SECRET']
+      }
+
+      auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
+  end
+
+  def user_credentials(auth_params)
+    header = {
+      Authorization: "Bearer #{auth_params['access_token']}"
+    }
+
+    user_response = RestClient.get('https://api.spotify.com/v1/me', header)
   end
 end
