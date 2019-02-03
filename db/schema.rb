d2debac07b8cfab98993c49e1c49512006aa60e2
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-03 02:19:19 -0200

    feat: brand new db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_172223) do
+ActiveRecord::Schema.define(version: 2019_02_03_013316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.string "href"
+    t.integer "popularity"
+    t.string "uri"
+    t.string "images"
+    t.string "spotify_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "artist_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_follows_on_artist_id"
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -26,4 +47,6 @@ ActiveRecord::Schema.define(version: 2019_02_02_172223) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "follows", "artists"
+  add_foreign_key "follows", "users"
 end
