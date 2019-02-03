85699e9b68a7ef3385a4d8756e0f3a74d84b5c19
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-02 23:09:00 -0200

    fix: changing redirection url

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::UsersController < ApplicationController
                                     )
       @user.update(access_token: auth_params['access_token'], refresh_token: auth_params['refresh_token'])
 
-      redirect_to 'http://localhost:3000/'
+      redirect_to 'https://spotify-your-artists.herokuapp.com'
     end
   end
 
